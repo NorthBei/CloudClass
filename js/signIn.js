f359cf102e252e129f72a97be2ba4fdcc0dd6fd0
NorthBei <seekingfor520@gmail.com>
2016-07-20 01:00:06 +0800

    react init version
    開始使用react套版
    新增並引入react libs
    
    -註冊會員
    react化完成
    原本的html並沒有刪除，只有註解掉

---
+++ js/signIn.js
@@ -0,0 +1,62 @@
+var SignInBox = React.createClass({
+    render: function() {
+        return (
+        <div id="sign_in_box">
+            <img id="close_box" src={"pic/X.png"} alt="關閉"/>
+            <h1 id="sign_in_title">註冊新會員</h1>
+            <h6 id="private_policy">隱私政策</h6>
+            <Email/>
+            <Password/>
+            <Buttons/>
+        </div>
+        );
+    }
+});
+
+var Email = React.createClass({
+    render: function() {
+        return (
+            <div className="input_component">
+                <label for="" className="input_intro">Email</label>
+                <div className="input_wrapper">
+                    <input type="email" className="input_area"/>
+                </div>
+            </div>
+        );
+    }
+});
+
+var Password = React.createClass({
+    render: function() {
+        return (
+            <div className="input_component">
+                <label for="" className="input_intro">密碼</label>
+                <div className="input_wrapper">
+                    <input type="password" className="input_area"/>
+                    <img id="eye_slash" src={"pic/eye-slash.png"} alt="切換密碼顯示"/>
+                </div>
+            </div>
+        );
+    }
+});
+
+var Buttons = React.createClass({
+    render: function() {
+        return (
+            <div id="buttons">
+                <div id="register_wrapper">
+                <span id="register">註冊</span>
+                </div>
+                <div id="or">或</div>
+                <div id="facebook_sign_in">使用Facebook進入</div>
+            </div>
+        );
+    }
+});
+
+window.addEventListener("load",function name(params) {
+    ReactDOM.render(
+        <SignInBox/>,
+        document.getElementById('sign_in_section')
+    );
+})
