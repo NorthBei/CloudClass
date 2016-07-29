931268a12a7097579279c03165cc3b2c6d886c2f
NorthBei <seekingfor520@gmail.com>
2016-07-29 20:46:12 +0800

    更改鈴鐺顯示/消失的function的使用方式
    -顯示/消失鈴鐺的function改成
    在parent component 透過一個id 拿到student的photo 在使用toogleBell
    Ex teacher.getStudent("zack").tooglebell();
    
    -移動程式碼
    tinyscroll的init function 移至 react StudentList class 的 componentDidMount
    function內

--- js/students.js
+++ js/students.js
@@ -1,6 +1,29 @@
+var users = [
+    {id:"photo_1",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_2",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_3",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_4",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_5",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_6",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_7",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_8",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_9",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_10",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_11",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_12",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_13",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_14",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_15",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_16",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_17",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_18",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"},
+    {id:"photo_19",photo:"http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg",msg:"我會好好執政，捍衛太平島主權，台灣爛波萬"},
+    {id:"photo_20",photo:"http://imgs.niusnews.com/upload/imgs/default/13AprCh/YagoPartal2013/1.jpg",msg:"有句話說：「人要衣裝，佛要金裝」，現在這句話可不僅能套在人類身上，動物們雖然也有毛皮的保護，但要是認真打扮起來可是一點都不輸給人類！"}
+];
+
 var Student = React.createClass({
 
-    toggleBell:function(event) {
+    toggleBell:function() {
       var bell = this.refs.bell;
 
       if(bell.style.visibility == "hidden" ){
@@ -38,12 +61,12 @@ var Student = React.createClass({
     },
     render: function() {
         return (
-            <div  ref="box"  className="student_phto_wrapper">
-                <img ref="photo" className="student_phto" src="http://img.ltn.com.tw/2016/new/jan/4/images/bigPic/400_400/php0lVqQo.jpg" alt="學生大頭貼"/>
+            <div  ref="box" id={this.props.id} className="student_phto_wrapper">
+                <img ref="photo" className="student_phto" src={this.props.photo} alt="學生大頭貼"/>
                 <img ref="bell" className="bell" src="pic/bell.png" alt="鈴鐺圖示"/>
                 <span ref="message" className="message_wrapper">
                     <div className="message_triangle"></div>
-                    <div className="message_box">我會好好執政，捍衛太平島主權，台灣爛波萬</div>
+                    <div className="message_box">{this.props.msg}</div>
                 </span>
             </div>
         );
@@ -51,30 +74,72 @@ var Student = React.createClass({
 });
 
 var StudentList = React.createClass({
+    getPhotoById:function(photoID) {
+        return this.refs[photoID];
+    },
+    componentDidMount:function (xxxx) {
+
+        //this.getPhotoById("photo_1").toggleBell();
+
+        var videoHeight =  document.getElementById("live_video").clientHeight;
+        console.log("height "+videoHeight);
+        //$("#students").css("height",videoHeight+"px");
+        //videoHeight-=60px;//60px是設定齒輪+padding的高度
+        $("#scrollbar1").css("height",(videoHeight-60)+"px");
+
+        var $scrollbar  = $('#scrollbar1')
+                ,   $overview   = $scrollbar.find(".overview")
+                ,   loadingData = false
+                ;
+
+        $scrollbar.tinyscrollbar({thumbSize : 60});
+
+        var scrollbarData = $scrollbar.data("plugin_tinyscrollbar")
+
+        $scrollbar.bind("move", function()
+        {
+            // The threshold will enable us to start loading the text before we reach the end.
+            //
+            var threshold       = 0.9
+            ,   positionCurrent = scrollbarData.contentPosition + scrollbarData.viewportSize
+            ,   positionEnd     = scrollbarData.contentSize * threshold
+            ;
+
+            // Check if have reached the "end" and that we arent allready in the process of loading new data.
+            //
+            if(!loadingData && positionCurrent >= positionEnd)
+            {
+                loadingData = true;
+
+                $.getJSON("data.json", function(data)
+                {
+                    loadingData = false;
+
+                    $overview.append(data.text)
+
+                    scrollbarData.update("relative");
+                });
+            }
+        });
+
+    },
     render: function() {
+        var users = this.props.users.map(function(user) {
+            return (
+                <Student key={user.id} ref={user.id} photo={user.photo} msg={user.msg}/>
+            );
+        });
+
         return (
             <div>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
-                <Student/>
+                {users}
             </div>
         );
     }
+
 });
 
 ReactDOM.render(
-    <StudentList/>,
+    <StudentList users={users}/>,
     document.getElementsByClassName("overview")[0]
 );
